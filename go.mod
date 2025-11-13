cc83bed73c120d2f74303655cc71e4f3a61652b8
Alexander Sustavov <markowillz@gmail.com>
2025-11-13 18:12:12 +0600

    Init project

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module oksana-vpn-telegram-bot
 
 go 1.25.4
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/telebot.v4 v4.0.0-beta.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"oksana-vpn-telegram-bot/internal/commands"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v4"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	pref := tele.Settings{
+		Token:  os.Getenv("BOT_TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+	fmt.Println("Token: " + os.Getenv("BOT_TOKEN"))
+
+	bot, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	commands.RegisterCommands(bot)
+
+	bot.Start()
+}
